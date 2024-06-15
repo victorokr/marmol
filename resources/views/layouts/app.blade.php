ed23a4f85ef96c4a477ee69ecc97742b6e86758c
victorokr <victorokr@hotmail.com>
2024-06-15 17:02:18 -0500

    eliminada etiqueta de google para seo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,15 +34,7 @@
         <meta name="twitter:creator"      content="@faroox" /> -->
         <meta name="theme-color"          content="#43149A">
 
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CH95TQSRMF"></script>
-        <!-- <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-CH95TQSRMF');
-        </script> -->
+       
  
         <!-- bootstrap css -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
